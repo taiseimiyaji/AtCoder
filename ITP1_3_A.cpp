1ac8386565a457a47aeab1d1b98d174b485ca9c9
taisei miyaji <82886655+taiseimiyaji@users.noreply.github.com>
2021-12-28 17:01:48 +0900

    Create ITP1_3_A.cpp
    Print Many Hello World

---
+++ ITP1_3_A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Main(){
+  //ITP1_3_A
+  //Print Many Hello World
+  for(int i = 0; i < 1000; i++){
+    cout << "Hello World" << endl;
+  }
+
+}
+
+int main(){
+  Main();
+}
