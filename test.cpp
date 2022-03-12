3acebc4407d8875261186c96363ce9f4409486ae
taisei miyaji <82886655+taiseimiyaji@users.noreply.github.com>
2022-03-12 17:05:39 +0900

    VScode設定

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <string.h>
+using namespace std;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+
+void Main(){ 
+// AtCoder Beginner Sellection
+// Welcome to AtCoder
+int a,b,c;
+string s;
+cin >> a >> b >> c >> s;
+int ans;
+ans = a + b + c;
+cout << ans << " ";
+cout << s << endl;
+
+
+}
+int main(){
+  Main();
+  return 0;
+}
